bd090a725ec35995b0236343a53f39a7f3e3e1a5
yosizumi1919 <kplmg2030@gmail.com>
2024-12-18 12:21:35 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
+  root to: "messages#index"
+end
+
+Rails.application.routes.draw do
+  get 'users/sign_up'
   root to: "messages#index"
 end
